4e10e967181368da79bcdaa8b3421ee20e8b71d4
EC2 Default User <ec2-user@ip-172-31-2-21.ec2.internal>
2022-05-18 05:10:45 +0000

    [add]投稿機能と一覧表示

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,4 +1,24 @@
 class BooksController < ApplicationController
   def top
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def show
+  end
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
